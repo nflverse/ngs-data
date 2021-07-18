b4b29ed219369ba2154a2f9b39f8f41d72bb5379
mrcaseb <roemerle@gmail.com>
2021-07-18 15:00:32 +0200

    initial commit

---
+++ R/ngs_functions.R
@@ -0,0 +1,185 @@
+`%>%` <- magrittr::`%>%`
+
+load_week_ngs <- function(season, week, type, session) {
+  # for testing
+  # season <- 2020
+  # week <- 5
+  # type <- "passing"
+  # session <- rvest::html_session("https://nextgenstats.nfl.com/stats/top-plays/fastest-ball-carriers")
+
+  if (week == 0) {
+    cli::cli_ul("Loading {season} overall {type} stats...")
+  } else {
+    cli::cli_ul("Loading {season} week {week} {type} stats...")
+  }
+
+  max_reg <- ifelse(season >= 2021, 18, 17)
+  min_post <- max_reg + 1
+  max_post <- min_post + 5
+
+  if (week == 0) {
+    path <- glue::glue("https://appapi.ngs.nfl.com/statboard/{type}?season={season}&seasonType=REG")
+  } else if (week %in% 1:max_reg) {
+    path <- glue::glue("https://appapi.ngs.nfl.com/statboard/{type}?season={season}&seasonType=REG&week={week}")
+  } else if (week %in% min_post:max_post) {
+    path <- glue::glue("https://appapi.ngs.nfl.com/statboard/{type}?season={season}&seasonType=POST&week={week}")
+  }
+
+  response <- httr::POST(
+    url = path,
+    session$config,
+    httr::config(referer = session$url),
+    httr::user_agent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.86 Safari/537.36"),
+    handle = session$handle
+  ) %>%
+    httr::content()
+
+  info <- response %>%
+    purrr::discard(is.list) %>%
+    tibble::as_tibble() %>%
+    janitor::clean_names() %>%
+    dplyr::mutate(week = week)
+
+  teams <- readRDS("R/teams_colors_logos.rds") %>%
+    dplyr::select(team_id, team_abbr) %>%
+    dplyr::filter(!team_abbr %in% c("LA", "OAK", "STL", "SD"))
+
+  if (!is.null(response %>% purrr::pluck("stats"))) {
+    stats <- response %>%
+      purrr::pluck("stats") %>%
+      purrr::map_dfr(function(x) {
+        st <- x %>%
+          purrr::discard(is.list) %>%
+          tibble::as_tibble_row()
+        pl <- x %>% purrr::pluck("player")
+        pl <- pl %>% purrr::set_names(paste0("player_", names(pl)))
+        return(dplyr::bind_cols(st, pl))
+      }) %>%
+      janitor::clean_names() %>%
+      dplyr::rename(
+        team_id = dplyr::first(tidyselect::contains("team_id"))
+      ) %>%
+      dplyr::left_join(teams, by = "team_id") %>%
+      dplyr::select(-tidyselect::any_of(c("season", "season_type", "week")))
+
+    out <- dplyr::bind_cols(info, stats) %>%
+      dplyr::select(
+        tidyselect::any_of(c("season", "season_type", "week")),
+        tidyselect::any_of(c("player_display_name", "player_position", "team_abbr")),
+        tidyselect::any_of(get(paste0(type, "_stats"))),
+        tidyselect::any_of(player_info)
+      ) %>%
+      dplyr::select(-tidyselect::any_of(c(
+        "player_nfl_id", "player_esb_id", "player_current_team_id",
+        "player_season", "player_football_name", "player_headshot",
+        "player_gsis_it_id", "player_ngs_position"
+      )))
+  } else {
+    out <- tibble::tibble()
+  }
+ out
+}
+
+save_ngs_data <- function(seasons) {
+  most_recent <- most_recent_season()
+  if (!all(seasons %in% 2016:most_recent)) {
+    cli::cli_abort("Please pass valid seasons between 2016 and {most_recent}")
+  }
+  session <- rvest::html_session("https://nextgenstats.nfl.com/stats/top-plays/fastest-ball-carriers")
+  todo <- expand.grid(season = seasons, type = c("passing", "rushing", "receiving"))
+  purrr::walk2(todo$season, todo$type, save_ngs_type, session)
+  cli::cli_alert_success("{.field DONE}")
+}
+
+save_ngs_type <- function(season, type = c("passing", "rushing", "receiving"), session) {
+  ngs <- purrr::map2_dfr(season, 0:22, load_week_ngs, type, session)
+  saveRDS(ngs, glue::glue("data/ngs_{season}_{type}.rds"))
+  readr::write_csv(ngs, glue::glue("data/ngs_{season}_{type}.csv.gz"))
+  qs::qsave(ngs, glue::glue("data/ngs_{season}_{type}.qs"),
+    preset = "custom",
+    algorithm = "zstd_stream",
+    compress_level = 22,
+    shuffle_control = 15
+  )
+}
+
+most_recent_season <- function() {
+  today <- Sys.Date()
+  current_year <- format(today, format = "%Y") %>% as.integer()
+  current_month <- format(today, format = "%m") %>% as.integer()
+  if (current_month >= 9){
+    return(current_year)
+  } else {
+    return(current_year - 1)
+  }
+}
+
+passing_stats <- c(
+  "avg_time_to_throw",
+  "avg_completed_air_yards",
+  "avg_intended_air_yards",
+  "avg_air_yards_differential",
+  "aggressiveness",
+  "max_completed_air_distance",
+  "avg_air_yards_to_sticks",
+  "attempts",
+  "pass_yards",
+  "pass_touchdowns",
+  "interceptions",
+  "passer_rating",
+  "completions",
+  "completion_percentage",
+  "expected_completion_percentage",
+  "completion_percentage_above_expectation",
+  "avg_air_distance",
+  "max_air_distance"
+)
+
+rushing_stats <- c(
+  "efficiency",
+  "percent_attempts_gte_eight_defenders",
+  "avg_time_to_los",
+  "rush_attempts",
+  "rush_yards",
+  "expected_rush_yards",
+  "rush_yards_over_expected",
+  "avg_rush_yards",
+  "rush_yards_over_expected_per_att",
+  "rush_pct_over_expected",
+  "rush_touchdowns"
+)
+
+receiving_stats <- c(
+  "avg_cushion",
+  "avg_separation",
+  "avg_intended_air_yards",
+  "percent_share_of_intended_air_yards",
+  "receptions",
+  "targets",
+  "catch_percentage",
+  "yards",
+  "rec_touchdowns",
+  "avg_yac",
+  "avg_expected_yac",
+  "avg_yac_above_expectation"
+)
+
+player_info <- c(
+  # "player_nfl_id",
+  # "player_esb_id",
+  "player_gsis_id",
+  "player_first_name",
+  "player_last_name",
+  # "player_position_group",
+  # "player_current_team_id",
+  "player_jersey_number",
+  # "player_status",
+  # "player_season",
+  # "player_football_name",
+  # "player_headshot",
+  # "player_gsis_it_id",
+  # "player_ngs_position",
+  # "player_ngs_position_group",
+  # "player_suffix",
+  "player_short_name"
+)
---
+++ R/update_ngs.R
@@ -0,0 +1,3 @@
+source("R/ngs_functions.R")
+
+save_ngs_data(most_recent_season())

1af5f4159dff34426b3bf0e235e621ef8901ffde
Tan Ho <tan@tanho.ca>
2024-10-06 12:34:06 +0100

    split into exec/update_ngs.R into one file per action

--- exec/update_ngs.R
+++ exec/update_ngs.R
@@ -1,10 +1,7 @@
 pkgload::load_all()
+
 season <- Sys.getenv("NFLVERSE_UPDATE_SEASON", unset = NA_character_) |> as.integer()
 type <- Sys.getenv("NFLVERSE_UPDATE_TYPE", unset = NA_character_)
-type <- rlang::arg_match0(type, c("passing", "rushing", "receiving", "combine"))
+type <- rlang::arg_match0(type, c("passing", "rushing", "receiving"))
 
-if (type == "combine"){
-  purrr::walk(c("passing", "rushing", "receiving"), combine_ngs_data)
-} else {
-  save_ngs_data(season, type)
-}
+save_ngs_data(season, type)
